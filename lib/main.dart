4e014dff5eb55334e392688f0205d2f4bc27e7e2
Anthony Kiplimo <anthonylimo90@gmail.com>
2018-06-07 10:55:45 +0300

    Scrollable list view
    This is some code adding a list view

--- lib/main.dart
+++ lib/main.dart
@@ -31,10 +31,49 @@ class RandomSentences extends StatefulWidget{
 }
 
 class _RandomSentencesState extends State<RandomSentences> {
+
+  final _sentences = <String>[];
+  final _biggerFont = const TextStyle(fontSize: 14.0);
+
+
   @override
   Widget build(BuildContext context){
     final noun = new WordNoun.random();
     final adjective =  new WordAdjective.random();
-    return  new Text("The quick ${noun.asCapitalized} jumped over the ${adjective.asCapitalized} ${noun.asCapitalized}");
+    return  new Text("");
+  }
+
+  String _getSentence(){
+    final noun = new WordNoun.random();
+    final adj = new WordAdjective.random();
+    return "The quick ${noun.asCapitalized} jumped over the ${adjective.asCapitalized} ${noun.asCapitalized}";
+  }
+
+  Widget _buildRow(String sentence) {
+    return new ListTile(
+      title: new Text(
+        sentence,
+        style: _biggerFont,
+      )
+    );
+  }
+
+  Widget _buildSentence(){
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i){
+        if(i.isOdd) return new Divider();
+
+        final index = i ~/ 2;
+
+        if(index >= _sentences.length){
+          for (int x=0; x< 10; x++){
+            _sentences.add(_getSentence());
+          }
+        }
+
+        return _buildRow(_sentences[index]);
+      },
+    );
   }
 }
